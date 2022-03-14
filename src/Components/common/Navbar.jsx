8618c2735c47ae62c0cc1c9f45740cb27f7468df
kevanultroneous <dev21.ultroneous@gmail.com>
2022-03-14 19:51:49 +0530

    commit

--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -26,8 +26,8 @@ const DivineNavbar = () => {
   return (
     <>
       <Image src={call} id="myBtn" onClick={() => setShow(true)} />
-      <Navbar expand="lg" 
-      style={{ background: chngClr ? '#FFEFE2' : 'none' ,transition:'0.5s'}} fixed="top">
+      <Navbar expand="lg"
+        style={{ background: chngClr ? '#FFEFE2' : 'none', transition: '0.5s' }} fixed="top">
         <Container fluid className="p-xl-3 p-md-3 p-lg-3 p-3">
           <Link to="/" className="text-decoration-none">
             <Navbar.Brand href="#" className="ms-md-5 ms-xs-1 ms-xl-5 ms-lg-5">
@@ -47,19 +47,19 @@ const DivineNavbar = () => {
               <Link to="/customtemples" className="text-decoration-none">
                 <Nav.Link href="/customtemples" className="me-xl-5 me-md-5 me-lg-5">Products</Nav.Link>
               </Link>
-              <Nav.Link href="http://localhost:3000/#about" className="">About Us</Nav.Link>
-              <div className="vector ms-5 me-3"/>
-              <Link to="/customtemples" className="text-decoration-none">
-                <Nav.Link href="/customtemples" className="fs-20">
-                  <FaFacebookF />
-                </Nav.Link>
-              </Link>
-              <Link to="/customtemples" className="text-decoration-none">
-                <Nav.Link className="fs-20">
-                  <AiFillInstagram />
-                </Nav.Link>
-              </Link>
-              <div className="vector me-5 ms-4"/>
+              <Nav.Link href="http://localhost:3000/#about" className="pt-xl-0 pt-lg-0">About Us</Nav.Link>
+              <div className="vector ms-5 me-3" />
+
+              <Nav.Link href="https://www.facebook.com/divinedecores/ " className="fs-20" target="_blank">
+                <FaFacebookF />
+              </Nav.Link>
+
+              <Nav.Link href="https://www.instagram.com/divinedecores/" className="fs-20" target="_blank">
+                <AiFillInstagram />
+              </Nav.Link>
+
+
+              <div className="vector me-5 ms-4" />
               <Nav.Link onClick={() => setShow(true)} className="p-0 me-xl-5" id="call-tab-mob">
                 <Image src={call} height="45" />
               </Nav.Link>
