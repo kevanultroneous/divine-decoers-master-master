e37e117e3d3066a1f932dfa0ed3c9034327896c1
kevanultroneous <dev21.ultroneous@gmail.com>
2022-03-15 17:08:14 +0530

    commit

--- src/Components/Home/TitleContent.jsx
+++ src/Components/Home/TitleContent.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const TitleContent = (props) => {
   return (
     <Row className="back-image under-content ">
-     <Col lg={6} xl={4} md={8} xs={12} className="ms-md-5">
-          <div className="title-level-1 fw-500 ">Bringing Your</div>
+     <Col lg={6} xl={4} md={8} xs={12} className="ms-md-5 Padding-Title">
+          <div className="title-level-1 fw-500 pt-5">Bringing Your</div>
           <div className="title-level-2 fw-700">Tradition Home</div>
           <div className="title-level-3 fw-500 me-xl-5 me-xs-0">
             <p>
--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Image, Nav, Navbar, Row } from "react-bootstrap";
+import { Col, Container, Image, Nav, Navbar, Offcanvas, Row } from "react-bootstrap";
 import { useState } from 'react'
 import { FaFacebookF } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
@@ -8,16 +8,18 @@ import OffCanvasComp from "./OffCanvasComp";
 import { Link, useNavigate } from "react-router-dom";
 import menu from "../../Assets/images/menu.png"
 import ScrollToTop from "react-scroll-to-top";
+import close from "../../Assets/images/close.png"
+
 const DivineNavbar = () => {
   const [show, setShow] = useState(false)
   const [chngClr, setChmgClr] = useState(false)
-  const [ navExpanded,setNavExpanded] = useState(false)
-
+  const [navExpanded, setNavExpanded] = useState(false)
+  const [offC, setOffc] = useState(false)
   const handleClose = () => {
     setShow(false)
   }
   const changeNavbarColor = () => {
-    if(navExpanded){
+    if (navExpanded) {
       setNavExpanded(false)
     }
     if (window.scrollY >= 80) {
@@ -31,10 +33,40 @@ const DivineNavbar = () => {
   return (
     <>
       <Image src={call} id="myBtn" onClick={() => setShow(true)} />
-      <Navbar 
-        onToggle={()=>navExpanded ? setNavExpanded(false) : setNavExpanded(true)}
-      expand="lg"
-       expanded={navExpanded}
+      <Offcanvas
+        show={offC}
+        onHide={() => setOffc(false)}
+        placement="end"
+        style={{ width: '250px', background: '#FFEFE2' }}>
+        <Offcanvas.Body>
+        <Row className="text-end mt-3 ">
+          <Col>
+            <Image src={close} height="30px" onClick={()=>setOffc(false)} />
+          </Col>
+        </Row>
+        <div className="mt-5">
+          <Nav.Link href="/" className="nav-link-clr me-xl-5 me-md-5 me-lg-5 fw-bold">
+            Home
+          </Nav.Link>
+          <Link to="/customtemples" className="text-decoration-none">
+            <Nav.Link href="/customtemples" className="me-xl-5 me-md-5 me-lg-5 fw-bold">Products</Nav.Link>
+          </Link>
+          <Nav.Link href="http://localhost:3000/#about" className="pt-xl-0 pt-lg-0 fw-bold">About Us</Nav.Link>
+
+          <Nav.Link href="https://www.facebook.com/divinedecores/ " className="fs-20" target="_blank">
+            <FaFacebookF />
+          </Nav.Link>
+
+          <Nav.Link href="https://www.instagram.com/divinedecores/" className="fs-20 pt-0 " target="_blank">
+            <AiFillInstagram />
+          </Nav.Link>
+        </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+      <Navbar
+        onToggle={() => setOffc(true)}
+        expand="lg"
+        expanded={navExpanded}
         style={{ background: chngClr ? '#FFEFE2' : 'none', transition: '0.5s' }} fixed="top">
         <Container fluid className="p-xl-3 p-md-3 p-lg-3 p-3">
           <Link to="/" className="text-decoration-none">
